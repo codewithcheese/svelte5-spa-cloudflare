15548ce436001eca57b9cd613d5d490597b31196
codewithcheese <mr.tmanning@gmail.com>
2024-05-03 23:22:34 +1000

    Add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+  type Store = {}
+
+  const store: Store = $state({})
+</script>
+
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
