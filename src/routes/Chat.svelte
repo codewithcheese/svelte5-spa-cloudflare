8e51e77f236f29ab1c8baeff9b6e76ff658b95b4
codewithcheese <mr.tmanning@gmail.com>
2024-05-04 00:39:14 +1000

    Add melt-ui

--- src/routes/Chat.svelte
+++ src/routes/Chat.svelte
@@ -1,10 +1,15 @@
 <script>
     import { useChat } from 'ai/svelte';
+    import Select from "../components/Select.svelte";
+
 
     const { input, handleSubmit, messages, error } = useChat();
 </script>
 
 <main>
+
+    <Select />
+
     <ul>
         {#each $messages as message}
             <li>{message.role}: {message.content}</li>
